40312d4588679587be3f69e74f554e35e444b9a5
Eyal <eyal@Eyals-MacBook-Pro-2.local>
2019-05-19 18:39:54 +0300

    commit before changes

--- triangel/triangel.js
+++ triangel/triangel.js
@@ -13,7 +13,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("my-slides");
   var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1} 
   if (n < 1) {slideIndex = slides.length}
@@ -23,7 +23,8 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block"; 
+  slides[slideIndex-1].style.display = "flex"; 
+  slides[slideIndex-1].classList.add("check");
   dots[slideIndex-1].className += " active";
 };
 
@@ -38,18 +39,44 @@ function showSlides(n) {
 
 /*  tryyyyyyyyyyyyyyyyyyyyyyyyyyy */
 
+// text //
+
 document.addEventListener("scroll", (event) => {
     var farFromTop = Math.abs(document.body.getBoundingClientRect().top);
     console.log(farFromTop);
     var oapcityText = document.querySelector(".text");
-    if(farFromTop > 80) {
+    if(farFromTop > 680) {
         oapcityText.style.opacity = 0;
     } else {
         oapcityText.style.opacity = 1;
     }
 });
 
+// arrow //
 
+document.addEventListener("scroll", (event) => {
+  var farFromTop = Math.abs(document.body.getBoundingClientRect().top);
+  console.log(farFromTop);
+  var opacityarrow = document.querySelector(".arrow1");
+  if(farFromTop > 500 && farFromTop < 600) {
+    opacityarrow.style.opacity = 1;
+} else {
+
+    opacityarrow.style.opacity = 0;
+}
+});
+
+
+// document.addEventListener("scroll", (event) => {
+//   var farFromTop = Math.abs(document.body.getBoundingClientRect().top);
+//   console.log(farFromTop);
+//   var opacityarrow2 = document.querySelector(".arrow2");
+//   if(farFromTop > 1000 && farFromTop > 900) {
+//     opacityarrow2.style.opacity = 1;
+// } else {
+//     opacityarrow2.style.opacity = 0;
+// }
+// });
 // JQUERY:
 
 // $(document).ready(function(){
@@ -66,3 +93,27 @@ document.addEventListener("scroll", (event) => {
 // END JQUERY
 
 
+
+
+// iphone
+
+var mySwiper = new Swiper ('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+})
